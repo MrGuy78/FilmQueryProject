584f2969972deab0f5fae7c69c64808ca903d0d3
Andrew Nelson <nels4766@gmail.com>
2024-12-06 17:24:40 -0700

    switch case 1

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -11,11 +11,12 @@ import com.skilldistillery.filmquery.entities.Film;
 public class FilmQueryApp {
 
 	DatabaseAccessor db = new DatabaseAccessorObject();
+	Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		FilmQueryApp app = new FilmQueryApp();
-		app.test();
-//		app.launch();
+//		app.test();
+		app.launch();
 	}
 
 	private void test() {
@@ -29,19 +30,47 @@ public class FilmQueryApp {
 	}
 
 	private void launch() {
-		Scanner input = new Scanner(System.in);
-
 		startUserInterface(input);
-
 		input.close();
 	}
 
 	private void startUserInterface(Scanner input) {
 		//TODO print menu
+		System.out.println("******** Film Inventory ********");
+		System.out.println("1. Find films by ID: ");
+		System.out.println("2. Find films by keyword: ");
+		System.out.println("3. Exit");
 		
+		String choice = input.nextLine();
+		
+	}
+		
+	private void userInterfaceChoice(String choice) {
+		switch (choice) {
+		case "1":
+			System.out.println("Enter the film's ID: ");
+			int filmId = input.nextInt();
+			Film film = db.findFilmById(filmId);
+			
+			if(film != null) {
+				System.out.println(film.getFilmTitle());
+				System.out.println(film.getReleaseYear());
+				System.out.println(film.getFilmRating());
+				System.out.println(film.getFilmDesc());
+			}
+			else {
+				System.out.println("No such film exists. Perhaps begin production yourself..?");
+			}
+		case "2":
+			System.out.println("Enter the film's keyword: ");
+		case "3":
+			System.out.println("Thanks for caring about film! Tiktok sucks! ");
+			break;
+		}
+	}
 		//TODO get choice
+		
 		//TODO act on choice
 		//TODO repeat till quit
-	}
 
 }
